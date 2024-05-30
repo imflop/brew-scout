1ff4b36e224f77c4f2434fbaf1e013cae078ce22
fl <imflop@gmail.com>
2024-05-30 09:57:24 +0300

    Dbg (#27)

--- brew_scout/libs/admin/backends.py
+++ brew_scout/libs/admin/backends.py
@@ -1,4 +1,5 @@
 import typing as t
+import logging
 
 from sqladmin.authentication import AuthenticationBackend
 from starlette.requests import Request
@@ -8,6 +9,7 @@ from starlette.responses import RedirectResponse
 class AdminAuthenticationBackend(AuthenticationBackend):
     def __init__(self, secret_key: str, client: t.Any):
         self.client = client
+        self.logger = logging.getLogger(__name__)
         super().__init__(secret_key)
 
     async def login(self, request: Request) -> bool:
@@ -20,6 +22,8 @@ class AdminAuthenticationBackend(AuthenticationBackend):
     async def authenticate(self, request: Request) -> bool | RedirectResponse:
         if not request.session.get("user"):
             redirect_uri = request.url_for("login_google")
+            self.logger.info(f"Redirect uri: {redirect_uri}", extra={"uri_type": str(type(redirect_uri))})
+
             return await self.client.authorize_redirect(request, redirect_uri)
 
         return True
